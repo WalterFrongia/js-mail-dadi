4ce7632628a176887fdc63823607fe4c24e64e13
WalterFrongia <walter.frongia@libero.it>
2021-08-06 17:54:45 +0200

    Finalizzazione parte email con implementazione messaggio per utente con verifica email correttaa o emai non corretta

--- myscript.js
+++ myscript.js
@@ -3,8 +3,8 @@
 
 // 1. Chiedere email all'utente. 
 
-let userEmail = prompt("Inserisci la tue email")
-document.getElementById("email").innerHTML = userEmail
+let userEmail = prompt("Inserisci la tue email");
+document.getElementById("email").innerHTML = userEmail;
 
 // 2. Creare una lista con delle email certificate che possano accedere al sito.
 
@@ -14,10 +14,19 @@ console.log(emailSaveSistem);
 
 // 3. Creare un messaggio appropriato per l'utente.
 
-if(userEmail != emailSaveSistem[0]){
-    
-    alert("ERRORE QUESTA EMAIL NON E' NEL SISTEMA")
-
-} else{
-    alert("EMAIL CORRETTA")
+let trovato = false;
+for(let i =0; i<4; i++){
+    console.log(i);
+    console.log(emailSaveSistem[i]);
+    let emailCorrente = emailSaveSistem[i]
+    if( emailCorrente == userEmail){
+        trovato = true;
+    }
 }
+ console.log(trovato);
+
+ if(trovato == true ){
+     alert("CORRETTO");
+ }else{
+     alert("NON CORRETTO");
+ }
