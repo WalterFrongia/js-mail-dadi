82acb52657a7aa01d2e43c840fc076f6f5210574
WalterFrongia <walter.frongia@libero.it>
2021-08-06 15:58:00 +0200

    Implementazione richiesta email all'utente

--- myscript.js
+++ myscript.js
@@ -2,5 +2,10 @@
 // SEZIONE EMAIL
 
 // 1. Chiedere email all'utente. 
+
+let userEmail = prompt("Inserisci la tue email")
+document.getElementById("email").innerHTML = userEmail
+
+
 // 2. Creare una lista con delle email certificate che possano accedere al sito.
 // 3. Creare un messaggio appropriato per l'utente.
