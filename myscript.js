ac6a81375598ed61a7ff5957964e8862c4da96fc
WalterFrongia <walter.frongia@libero.it>
2021-08-06 16:30:34 +0200

    Aggiunto messaggio per utente

--- myscript.js
+++ myscript.js
@@ -6,6 +6,17 @@
 let userEmail = prompt("Inserisci la tue email")
 document.getElementById("email").innerHTML = userEmail
 
-
 // 2. Creare una lista con delle email certificate che possano accedere al sito.
+
+let emailSaveSistem = [ "prima.email@libero.it", "seconda.email@libero.it", "terza.email@libero.it", "quarta.email@libero.it"];
+console.log(emailSaveSistem);
+
+
 // 3. Creare un messaggio appropriato per l'utente.
+
+if(userEmail != emailSaveSistem){
+    alert("ERRORE QUESTA EMAIL NON E' NEL SISTEMA")
+
+} else if (userEmail == emailSaveSistem){
+    alert("EMAIL CORRETTA")
+}
