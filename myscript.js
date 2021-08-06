fe8cce09e000c8fe52695f4e81768954349196d5
WalterFrongia <walter.frongia@libero.it>
2021-08-06 15:47:02 +0200

    Inizializzazione progetto email con impostazione basica dei tre file html, css e js

---
+++ myscript.js
@@ -0,0 +1,6 @@
+
+// SEZIONE EMAIL
+
+// 1. Chiedere email all'utente. 
+// 2. Creare una lista con delle email certificate che possano accedere al sito.
+// 3. Creare un messaggio appropriato per l'utente.
