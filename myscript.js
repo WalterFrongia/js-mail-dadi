3534e5bfe1de1cf6d0a41ded5765b316300695c4
WalterFrongia <walter.frongia@libero.it>
2021-08-25 17:27:27 +0200

    Completato esercizio numero randomico

--- myscript.js
+++ myscript.js
@@ -30,3 +30,30 @@ for(let i =0; i<4; i++){
  }else{
      alert("NON CORRETTO");
  }
+
+
+//  SEZIONE GIOCO DEI DADI
+
+// Generare un numero random da 1 a 6.
+
+// sia per il giocatore o giocatrice sia per il computer.
+
+// Stabilire il vincitore o la vincitrice, in base a chi fa il punteggio più alto.
+
+let numeroGiocatore = parseInt (prompt ("inserisci un numero da 1 a 6") );
+console.log(numeroGiocatore);
+
+let numeroComputer =  Math.floor(Math.random() * 6) + 1 ;
+console.log(numeroComputer);
+
+if(numeroGiocatore > 6){
+    alert("Limite numero 6")
+}else{
+    if(numeroGiocatore > numeroComputer){
+        console.log("ha vinto il giocatore");
+    }else if( numeroComputer == numeroGiocatore){
+        console.log("avete pareggiato");
+    }else{
+        console.log("ha vinto il computer");
+    }
+}
